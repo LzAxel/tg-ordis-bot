4d9d0c4467d546ac05f556f2dde103254e2f2cd8
LzAxel <lzaxelmail@mail.com>
2022-03-05 17:22:11 +0200

    - Translated to English (almost). - Using Pydantic for work with API - More better architecture

---
+++ run.py
@@ -0,0 +1,21 @@
+from bot import dp
+from main import check_new_alerts, check_new_articles
+
+from handlers import register_handlers
+from aiogram import executor
+import asyncio
+
+
+async def async_tasks(*args):
+    loop = asyncio.get_event_loop()
+    loop.create_task(check_new_articles())
+    loop.create_task(check_new_alerts())
+
+
+def main():
+    register_handlers(dp)
+    executor.start_polling(dp, skip_updates=True, on_startup=async_tasks)
+
+
+if __name__ == '__main__':
+    main()
